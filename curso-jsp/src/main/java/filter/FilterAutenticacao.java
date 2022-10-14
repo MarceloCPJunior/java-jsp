2e34b677b6a091981d40ed89e58b2f97a4ac686d
Zabra <Zabra@DESKTOP-89LK0FU>
2022-10-14 00:13:13 -0300

    Projeto-jsp

---
+++ curso-jsp/src/main/java/filter/FilterAutenticacao.java
@@ -0,0 +1,81 @@
+package filter;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import connection.SingleConnectionBanco;
+
+@WebFilter(urlPatterns = {"/principal/*"})
+public class FilterAutenticacao extends HttpFilter implements Filter {
+	
+	private static Connection connection;
+	
+	public FilterAutenticacao() {
+	}
+
+	public void destroy() {
+		try {
+			connection.close();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		try { 
+			HttpServletRequest req = (HttpServletRequest) request;
+			HttpSession session = req.getSession();
+			
+			String usuarioLogado = (String) session.getAttribute("usuario");
+			
+			String urlParaAutenticar = req.getServletPath();/*Url que está sendo acessada*/
+			
+			/*Validar se está logado senão redireciona para a tela de login*/
+			if (usuarioLogado == null  && 
+					!urlParaAutenticar.equalsIgnoreCase("/principal/ServletLogin")) {/*Não está logado*/
+				
+				RequestDispatcher redireciona = request.getRequestDispatcher("/index.jsp?url=" + urlParaAutenticar);
+				request.setAttribute("msg", "Por favor realize o login!");
+				redireciona.forward(request, response);
+				return; /*Para a execução e redireciona para o login*/
+				
+			}else {
+				chain.doFilter(request, response);
+			}
+			
+			connection.commit();/*Deu tudo certo, então comita as alteracoes no banco de dados*/
+		
+	    }catch (Exception e) {
+			e.printStackTrace();
+			
+			RequestDispatcher redirecionar = request.getRequestDispatcher("erro.jsp");
+			request.setAttribute("msg", e.getMessage());
+			redirecionar.forward(request, response);
+			
+			try {
+				connection.rollback();
+			} catch (SQLException e1) {
+				e1.printStackTrace();
+			}
+		}
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		connection = SingleConnectionBanco.getConnection();
+	}
+
+}
